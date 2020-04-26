30fb6227be4fc8b6a3def74e54789b99194c214e
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 02:32:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 
-def fizzbuzz
+def fizzbuzz (int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+
 
 end
 # Don't forget! This file needs to be 'required' in its spec file
